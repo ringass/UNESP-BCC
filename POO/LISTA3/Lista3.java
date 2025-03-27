8d90af1687f5e5456f18639aece4bb554b4eaa18
ringass <murilogonzaga115@gmail.com>
2025-03-27 16:56:56 -0300

    continuando trabai do paiola

--- POO/LISTA3/Lista3.java
+++ POO/LISTA3/Lista3.java
@@ -14,12 +14,32 @@ abstract class Usuario {
 
 class Cliente extends Usuario {
 
+    Cliente(String nome, String email, int id) {
+        this.nome = nome;
+        this.email = email;
+        this.id = id;
+    }
+
+    @Override
+    public void visualizarPerfil() {
+        System.out.println("-- Dados do Cliente --");
+        System.out.format("Nome: %s\nEmail: %s\nId: %d", nome, email, id);
+    }
+
 }
 
 class Entregador extends Usuario {
 
     String meioDeTransporte;
-    boolean statusDisponivel;
+    boolean status;
+
+    Entregador(String nome, String email, int id, boolean status, String meioDeTransporte) {
+        this.nome = nome;
+        this.email = email;
+        this.id = id;
+        this.status = status;
+        this.meioDeTransporte = meioDeTransporte;
+    }
 
 }
 
@@ -27,31 +47,46 @@ class Restaurante extends Usuario {
     String nomeRestaurante;
     List<String> cardapio[];
 
+    Restaurante(String nome, String email, int id, String nomeRestaurante) {
+        this.nome = nome;
+        this.email = email;
+        this.id = id;
+        this.nomeRestaurante = nomeRestaurante;
+    }
 }
 
 class Pedido {
+
     Cliente cliente = new Cliente();
     Restaurante restaurante = new Restaurante();
     Entregador entregador = new Entregador();
     public double valor;
-    List<String> cardapio = new ArrayList<String>();
+    List<String> itens = new ArrayList<String>();
 
-    enum status {
-        REALIZADO, EM_PREPARO, ENTREGUE
+    enum Status {
+        REALIZADO, EM_PREPARO, ENTREGUE;
     };
 
-    public void atualizarStatus() {
+    Status status = Status.EM_PREPARO;
 
+    public void atualizarStatus(Status newStatus) {
+        status = newStatus;
     }
 
     public void atribuirEntregador(Entregador cabrunco) {
-
+        entregador = cabrunco;
     }
 
     public void resumoDoPedido() {
-
+        System.out.printf("Nome: %s" +
+                "Restaurante: %s", cliente.nome, restaurante.nomeRestaurante);
+        for (String p : itens) {
+            System.out.println(p);
+        }
+        System.out.println("Entregador: " + entregador.nome);
+        System.out.println("Valor do Pedido: " + valor);
+        System.out.println("Status: " + status);
     }
-
 }
 
 class SistemaDelivery {
@@ -61,20 +96,91 @@ class SistemaDelivery {
     List<Restaurante> restaurantes = new ArrayList<Restaurante>();
     List<Pedido> pedidos = new ArrayList<Pedido>();
 
-    public void Cadastrar() {
+    public void Cadastrar(int x, Scanner sc) {
+
+        if (x == 1) {
+            System.out.println("== CADASTRO DE CLIENTE ==");
+            System.out.println("Digite o nome completo: ");
+            String newName = sc.nextLine();
+            System.out.println("Digite o email: ");
+            String newEmail = sc.nextLine();
+
+            int newId = clientes.size() + 1;
+
+            Cliente newCliente = new Cliente(newName, newEmail, newId);
+
+            clientes.add(newCliente);
+
+        } else if (x == 2) {
+
+            int newId = restaurantes.size() + 1;
+
+            System.out.println("== CADASTRO DE RESTAURANTE ==");
+            System.out.println("Digite o nome do proprietario: ");
+            String newName = sc.nextLine();
+            System.out.println("Digite o nome do restaurante: ");
+            String newName2 = sc.nextLine();
+            System.out.println("Digite o email do restaurante: ");
+            String newEmail = sc.nextLine();
+
+            Restaurante newRestaurante = new Restaurante(newName, newEmail, newId, newName2);
+
+            restaurantes.add(newRestaurante);
+
+        } else {
+
+            int newId = entregadores.size() + 1;
+
+            System.out.println("== CADASTRO DE ENTREGADOR ==");
+            System.out.println("Digite o nome completo: ");
+            String newName = sc.nextLine();
+            System.out.println("Digite o email: ");
+            String newEmail = sc.nextLine();
+            System.out.println("Digite o meio de transporte(Carro, Moto, Bicicleta): ");
+            String newMeio = sc.nextLine();
+
+            Entregador newEntregador = new Entregador(newName, newEmail, newId, false, newMeio);
+
+            entregadores.add(newEntregador);
+        }
+
+    }
+
+    public void ShowRestaurantes(){
+
+        for(Restaurante r : restaurantes){
+            ShowRestaurantes(r);
+        }
 
     }
 
     public void CriarPedido() {
-
+        System.out.println("FORMULARIO PARA REALIZAR PEDIDO");
+        System.out.println("Selecione um Restaurante:");
+        
+        
+        
     }
 
     public void verificarDisponibilidade() {
 
     }
 
-    public void pedidos(int x) {
+    public void pedidos(int x, Scanner sc) {
+        if(x == 1){
+            System.out.println("Qual o ID do pedido?");
+            int id = sc.nextInt();
 
+            pedidos.get(id).resumoDoPedido();
+        }else{
+            
+            System.out.println("LISTA DOS PEDIDOS: ");
+
+            for(int i = 0; i < pedidos.size(); i++){
+                System.out.printf("Pedido %d", i);
+                pedidos.get(i).resumoDoPedido();
+            }
+        }
     }
 
 }
@@ -102,9 +208,11 @@ public class Lista3 {
             escolha = sc.nextInt();
 
             while (escolha < 1 || escolha > 8) {
-                System.out.println("As Escolhas vao de 1 até 8, por favor digite sua escolha: ");
+                System.out.println("As Escolhas vao de 1 ao 8, por favor digite sua escolha: ");
                 escolha = sc.nextInt();
             }
+
+            sc.nextLine();
 
             switch (escolha) {
 
@@ -112,7 +220,13 @@ public class Lista3 {
 
                 }
                 case (2): {
-
+                    System.out.println("== CADASTRO DE RESTAURANTE ==");
+                    System.out.println("Digite o nome do proprietario: ");
+                    String newName = sc.nextLine();
+                    System.out.println("Digite o nome do restaurante: ");
+                    String newName2 = sc.nextLine();
+                    System.out.println("Digite o email do restaurante: ");
+                    String newEmail = sc.nextLine();
                 }
                 case (3): {
 
@@ -120,7 +234,6 @@ public class Lista3 {
                 case (4): {
 
                 }
-
                 case (5): {
 
                 }
@@ -142,14 +255,14 @@ public class Lista3 {
         sc.close();
     }
 
-
-    public static void clrscr(){
-        //Clears Screen in java
+    public static void clrscr() {
+        // Clears Screen in java
         try {
             if (System.getProperty("os.name").contains("Windows"))
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             else
                 Runtime.getRuntime().exec("clear");
-        } catch (IOException | InterruptedException ex) {}
+        } catch (IOException | InterruptedException ex) {
+        }
     }
 }
